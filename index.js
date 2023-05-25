ba2ee4f6b38659f4b2d5138de4be44e2553dbbf5
unknown <129460051+DimeDimitrov@users.noreply.github.com>
2023-05-25 22:35:34 +0200

    Added icon and add quality options

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const path = require("path");
 const videoIds = [
   "ybS7AHGoEI8",
   "2VXacYLcjGA",
-  "rvcaEzUtcWk",
   "_nQU_8Nm0Yk",
   "T8r3cWM4JII",
   "V7HdWeYbV3Q",
 ];
+
+const volume = 0.2;
+const quality = "small"; // OOptions are hd1080, hd720, medium, small
+const playbackSpeed = 1.5;
 
 function getRandomVideoId() {
   const randomIndex = Math.floor(Math.random() * videoIds.length);
@@ -23,6 +26,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, "icon.ico"),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
@@ -56,6 +60,7 @@ ipcMain.on("open-windows", (event, count) => {
     let childWindow = new BrowserWindow({
       width: 400,
       height: 300,
+      icon: path.join(__dirname, "icon.ico"),
       webPreferences: {
         nodeIntegration: false,
         contextIsolation: true,
@@ -68,8 +73,9 @@ ipcMain.on("open-windows", (event, count) => {
     childWindow.webContents.on("did-finish-load", () => {
       const videoId = getRandomVideoId();
       childWindow.webContents.executeJavaScript(`
+      
         const iframe = document.createElement('iframe');
-        iframe.src = 'https://www.youtube.com/embed/${videoId}';
+        iframe.src = 'https://www.youtube.com/embed/${videoId}?vq=${quality}';
         iframe.style.width = '100%';
         iframe.style.height = '100%';
         iframe.style.position = 'absolute';
@@ -77,12 +83,17 @@ ipcMain.on("open-windows", (event, count) => {
         iframe.style.left = '0';
         iframe.allow = 'autoplay; encrypted-media; picture-in-picture';
         iframe.allowFullscreen = true;
-        iframe.setAttribute('allowfullscreen', '');
-        iframe.setAttribute('frameborder', '0');
-        iframe.setAttribute('autoplay', '1');
+        iframe.frameborder = 0;
+        iframe.volume = ${volume}
         document.body.style.margin = '0';
         document.body.style.overflow = 'hidden';
         document.body.appendChild(iframe);
+        iframe.onload = function() {
+          const player = iframe.contentWindow.document.querySelector('video');
+          if (player) {
+            player.playbackRate = ${playbackSpeed};
+          }
+        };
       `);
     });
 
